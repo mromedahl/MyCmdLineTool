c69951f3910ab15cdb3b7fae01cbb94dbec1fe19
mromedahl <magnus.romedahl@gmail.com>
2022-02-22 07:57:52 +0100

    Update main.c

--- MyCmdLineTool/main.c
+++ MyCmdLineTool/main.c
@@ -11,7 +11,7 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     char MyInput[100];
     
-    printf("Hej skriv namn:");
+    printf("Hej skriv namn: ");
     scanf("%s", MyInput);
     printf("Hej %s!\n", MyInput);
     return 0;
